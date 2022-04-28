dd816baf4c9ea80fd3d261b6038d8458df7c24e5
16602629703 <16602629703@163.com>
2022-04-28 11:37:05 +0800

    .

--- src/main/java/com/spring/demo/aop/GPCglibAopProxy.java
+++ src/main/java/com/spring/demo/aop/GPCglibAopProxy.java
@@ -1,10 +1,16 @@
 package com.spring.demo.aop;
 
+import java.lang.reflect.Method;
+import java.util.List;
+
+import net.sf.cglib.proxy.InvocationHandler;
+import net.sf.cglib.proxy.Proxy;
+
 /**
  * 使用CGlib API生成代理类，在此不举例
  * 感兴趣的可以自行实现
  */
-public class GPCglibAopProxy implements GPAopProxy {
+public class GPCglibAopProxy implements GPAopProxy, InvocationHandler {
     private GPAdvisedSupport config;
 
     public GPCglibAopProxy(GPAdvisedSupport config) {
@@ -13,11 +19,24 @@ public class GPCglibAopProxy implements GPAopProxy {
 
     @Override
     public Object getProxy() {
-        return null;
+        return getProxy(this.config.getTargetClass().getClassLoader());
     }
 
     @Override
     public Object getProxy(ClassLoader classLoader) {
-        return null;
+        return Proxy.newProxyInstance(classLoader, this.config.getTargetClass().getInterfaces(), this);
     }
+
+    //invoke()方法是执行代理的关键入口
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        //将每一个JoinPoint也就是被代理的业务方法（Method）封装成一个拦截器，组合成一个拦截器链
+        List<Object> interceptorsAndDynamicMethodMatchers =
+                config.getInterceptorsAndDynamicInterceptionAdvice(method, this.config.getTargetClass());
+        //交给拦截器链MethodInvocation的proceed()方法执行
+        GPMethodInvocation invocation = new GPMethodInvocation(proxy, this.config.getTargetClass(),
+                method, args, this.config.getTargetClass(), interceptorsAndDynamicMethodMatchers);
+        return invocation.proceed();
+    }
+    
 }
--- src/main/java/com/spring/demo/test/service/impl/QueryService.java
+++ src/main/java/com/spring/demo/test/service/impl/QueryService.java
@@ -2,20 +2,17 @@ package com.spring.demo.test.service.impl;
 
 import com.spring.demo.mvc.annotation.GPService;
 import com.spring.demo.test.service.IQueryService;
-import lombok.extern.slf4j.Slf4j;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @GPService
-@Slf4j
 public class QueryService implements IQueryService {
     @Override
     public String query(String name) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String time = sdf.format(new Date());
         String json = "{name:\"" + name + "\",time:\"" + time + "\"}";
-        log.info("这是在业务方法中打印的：" + json);
         return json;
     }
 }
